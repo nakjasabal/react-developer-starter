c6a225c1e99829f007dfb5f5197b594f6bb0fea7
nakjasabal <nakjasabal@naver.com>
2025-11-23 13:52:55 +0900

    .env파일 수정

--- React/react06-hook-enhanced/src/App.jsx
+++ React/react06-hook-enhanced/src/App.jsx
@@ -1,14 +1,15 @@
 import {Routes, Route} from "react-router-dom";
 import TopNavi from './components/TopNavi';
+import TestForm from './components/TestForm';
 import UseOptimisticExam from './components/UseOptimisticExam';
-import UseFormStatusExam from './components/UseFormStatusExam';
 import UseActionStateExam from './components/UseActionStateExam';
+import UseFormStatusExam from './components/UseFormStatusExam';
 
 function App() {
   return (<>
     <TopNavi></TopNavi>
     <Routes>
-      <Route path='/' element={<UseOptimisticExam/>} />
+      <Route path='/' element={<TestForm />} />
       <Route path='/use-optimistic' element={<UseOptimisticExam />} />      
       <Route path='/use-action-state' element={<UseActionStateExam />} />
       <Route path='/use-form-status' element={<UseFormStatusExam />} />
